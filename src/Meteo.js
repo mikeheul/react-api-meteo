8fb6ea6dc1f08d2170324319ceeef72c8f13db1a
Mikeheul <mickael.murmann@gmail.com>
2022-07-02 14:42:12 +0200

    init commit

---
+++ src/Meteo.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import moment from 'moment';
+import 'moment/locale/fr';
+
+const Meteo = () => {
+    
+    const [currentLocation, setCurrentLocation] = useState([]) 
+    const [meteoData, setMeteoData] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get('https://geolocation-db.com/json/')
+            .then((res) => setCurrentLocation(res.data))
+            // .then((res) => console.log(res.data);
+    }, [])
+
+    const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+    
+    useEffect(() => {
+        axios
+            .get(`http://api.weatherstack.com/current?access_key=&query=${API_KEY}`)
+            .then((res) => setMeteoData(res.data))
+            // .then((res) => console.log(res.data))
+    }, [])
+
+    moment.locale('fr') 
+    
+    return (
+        <div className='card'>
+            <div className="main-content">
+                <span className="date">{ moment().format("dddd Do MMMM YYYY") }</span>
+                <h1><i className="fa-solid fa-location-dot"></i> {currentLocation.city}</h1>
+                <span className="region">{currentLocation.country_name + ', ' + currentLocation.state}</span><br />
+                {/* <span className="temperature">{meteoData.current.temperature}°C</span> */}
+                <span className="temperature">25°C</span>
+            </div>
+            <div className="secondary-content">
+                <p>
+                    <i className="fa-solid fa-wind"></i> 24 km/h<br />
+                    <i className="fa-solid fa-droplet"></i> 10 mm<br />
+                    <i className="fa-solid fa-gauge"></i> 10 % <br />
+                </p>
+                <img src="../sunny.png" alt="" />
+            </div>
+        </div>
+
+    );
+};
+
+export default Meteo;
